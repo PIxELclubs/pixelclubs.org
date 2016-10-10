2755ea46ce0c4ba89e91c6970f3e7441dcb6af08
Timothy Gu <timothygu99@gmail.com>
2016-10-09 23:14:34 -0700

    upload: Do not delete news.json

--- upload.sh
+++ upload.sh
@@ -10,7 +10,7 @@ git clone 'https://github.com/PIxELclubs/pixelclubs.github.io.git' web
 cd web
 git config credential.helper "store --file=.git/credentials"
 echo "https://${GH_TOKEN}@github.com" > .git/credentials
-git ls-files | grep -Fvx CNAME | xargs rm -rf
+git ls-files | grep -Fvx CNAME | grep -Fvx news.json | xargs rm -rf
 cp -a ../out/* .
 git commit -a -m 'Update'
 git push
